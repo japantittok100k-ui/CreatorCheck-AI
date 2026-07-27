4155b4aad20635ed5e4046abe4875fa8f723ff3c
japantittok100k-ui <japantittok100k@gmail.com>
2026-07-27 13:17:33 +0630

    Update server.js

--- server.js
+++ server.js
@@ -146,9 +146,9 @@ res.end(JSON.stringify({
 }));
 
 
-        res.writeHead(200,{
-          "Content-Type":"application/json; charset=utf-8"
-        });
+        
+  
+      
 
 
         res.end(text);
